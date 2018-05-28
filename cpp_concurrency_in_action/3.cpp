3434a9452d96cf0f3b023a00ad94d7fdb766198c
3344aaxgl <343070926@qq.com>
2018-05-28 21:24:25 +0800

    2018-05-28线程间共享数据
    线程间共享数据

--- cpp_concurrency_in_action/3.cpp
+++ cpp_concurrency_in_action/3.cpp
@@ -136,7 +136,7 @@ public:
     friend void swap(X& lhs, X& rhs)
     {
         std::lock(lhs.m, rhs.m);
-        std::lock_guard<std::mutex> lock_a(lhs.m,std::adopt_lock);
+        std::lock_guard<std::mutex> lock_a(lhs.m,std::adopt_lock);//只支持adopt_lock
         std::lock_guard<std::mutex> lock_b(rhs.m,std::adopt_lock);
 
         std::swap(lhs.data, rhs.data);
@@ -146,8 +146,101 @@ public:
 //层级互斥量
 class hierarchical_mutex
 {
+private:
+    std::mutex interal_mutex;
+    unsigned long const hierarchy_value;
+    unsigned long previous_hierarchy_value;
+    static thread_local unsigned long this_thread_hierarchy_value;
+    void check_for_hierarchy_violation()
+    {
+        if(this_thread_hierarchy_value <= hierarchy_value)//限制锁的层级
+        {
+            throw std::logic_error("mutex hierarchy violated");
+        } 
+    }
 
+    void update_hierarchy_value()
+    {
+        previous_hierarchy_value = this_thread_hierarchy_value;
+        this_thread_hierarchy_value = hierarchy_value;
+    }
+
+public:
+    explicit hierarchical_mutex(unsigned long value):hierarchy_value(value),previous_hierarchy_value(0)
+    {
+
+    }
+
+    void lock()
+    {
+        check_for_hierarchy_violation();//先检查当前的层次是否大于要锁的层次
+        interal_mutex.lock();
+        update_hierarchy_value();//更新当前层次为锁住的层次
+    }
+
+    void unlock()
+    {
+        this_thread_hierarchy_value = previous_hierarchy_value;//回退到上一个层次
+        interal_mutex.unlock();
+    }
+
+    bool try_lock()
+    {
+        check_for_hierarchy_violation();
+        if(!interal_mutex.try_lock())
+          return false;
+        update_hierarchy_value();
+        return true;
+    }
 };
+
+//每个线程都能独立的拥有这个变量的副本，结合static一起使用，在这个线程中，这个类的所有对象都能按照顺序进行上锁
+thread_local unsigned long hierarchical_mutex::this_thread_hierarchy_value(ULONG_MAX);
+
+
+void Test_hierarchical_mutex()
+{
+    hierarchical_mutex  high_level_lock(1000);
+    hierarchical_mutex  low_level_lock(500);
+    std::lock_guard<hierarchical_mutex> lk1(high_level_lock);//调整顺序报错
+    std::lock_guard<hierarchical_mutex> lk2(low_level_lock);
+    
+    std::cout<<"lock two level lock"<<'\n';
+}
+
+class X1
+{
+private:
+    int data;
+    std::mutex m;
+public:
+    X1(int d):data(d)
+    {
+
+    }
+    friend void swap(X1& lhs, X1& rhs)
+    {
+        std::unique_lock<std::mutex> lock_a(lhs.m,std::defer_lock);//仍未取得锁，只是方便解锁
+        std::unique_lock<std::mutex> lock_b(rhs.m,std::defer_lock);
+        std::lock(lock_a, lock_b);//互斥量上锁
+
+        std::swap(lhs.data, rhs.data);
+    }
+};
+
+std::unique_lock<std::mutex> get_lock()
+{
+    //引用全局变量
+    extern std::mutex some_mutex;
+    std::unique_lock<std::mutex> lk(some_mutex);
+    return lk;
+}
+
+void process_data()
+{
+    std::unique_lock<std::mutex> lk(get_lock());
+}
+
 
 int main()
 {
@@ -161,6 +254,8 @@ int main()
     for_each(threads.begin(),threads.end(),std::mem_fn(&std::thread::join));
 
     std::cout<<list_contains(4)<<" "<<list_contains(3);
+
+    Test_hierarchical_mutex();
 
     return 0;
 }
