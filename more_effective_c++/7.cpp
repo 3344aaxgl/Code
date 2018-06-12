9c3c04da57b0ff2648b8788f722e92cca0d7bcb2
3344aaxgl <343070926@qq.com>
2018-06-12 20:07:02 +0800

    2018-06-12计数引用
    计数引用

--- more_effective_c++/7.cpp
+++ more_effective_c++/7.cpp
@@ -487,6 +487,7 @@ private:
         char* data;
         int refCount;
         stringvalue(const char* initvalue = "");
+        bool shareable;
         ~stringvalue();
     };
     stringvalue* value;
@@ -494,6 +495,7 @@ public:
     my_string(const char* initvalue = ""):value(new stringvalue(initvalue))
     {
         ++value->refCount;
+        value->shareable = true;
     }
     
     ~my_string()
@@ -502,9 +504,13 @@ public:
             delete value;
     }
 
-    my_string(const my_string& ms):value(ms.value)
+    my_string(const my_string &ms)
     {
-         ++value->refCount;
+        if (!ms.value->shareable)
+            value = ms.value;
+        else
+            value = new stringvalue(ms.value->data);
+        ++value->refCount;
     }
 
     my_string& operator=(const my_string& ms)
@@ -513,7 +519,10 @@ public:
             return *this;
         if(--value->refCount == 0)
             delete value;
-        value = ms.value;
+        if(ms.value->shareable)
+           value = ms.value;
+        else
+           value = new stringvalue(ms.value->data);
         ++value->refCount;
         return *this;
     }
@@ -530,6 +539,7 @@ public:
             --value->refCount;
             value = new stringvalue(value->data);
         }
+        value->shareable = false;
         return value->data[index];
     }
 }; 
@@ -544,6 +554,241 @@ my_string::stringvalue::~stringvalue()
 {
     delete [] data;
 }
+
+class RCObject
+{
+  private:
+    int refCOunt;
+    bool shareable;
+
+  public:
+    RCObject() : refCOunt(0), shareable(true) {}
+
+    virtual ~RCObject() = 0;
+
+    void addreference()
+    {
+        ++refCOunt;
+    }
+
+    void removereference()
+    {
+        if(--refCOunt == 0)
+          delete this;
+    }
+
+    void markUnshareable()
+    {
+        shareable = false;
+    }
+
+    bool Isshareable()
+    {
+        return shareable;
+    }
+
+    bool Isshared()
+    {
+        return refCOunt > 1;
+    }
+};
+
+RCObject::~RCObject()
+{
+
+}
+
+template <typename T>
+class RCPtr
+{
+  private:
+    T *pointee;
+    void init();
+  public:
+    RCPtr(T *realPtr = 0):pointee(realPtr)
+    {
+        init();
+    }
+    RCPtr(const RCPtr &rhs):pointee(rhs.pointee)
+    {
+        init();
+    }
+    virtual ~RCPtr()
+    {
+        pointee->removereference();
+    }
+    RCPtr &operator=(const RCPtr &rhs)
+    {
+        if(pointee != rhs.pointee)
+        {
+            if(pointee)
+              pointee->removereference();
+            pointee = rhs.pointee;
+            init();
+        }
+        return *this;
+    }
+    T *operator->() const
+    {
+        return pointee;
+    }
+    T &operator*() const
+    {
+        return *pointee;
+    }  
+};
+
+template<typename T>
+void RCPtr<T>::init()
+{
+    if(pointee == 0)
+      return ;
+    if(pointee->Isshareable() == false)
+      pointee = new T(*pointee);
+    pointee->addreference();
+}
+
+class my_RCstring
+{
+  private:
+    struct stringvalue : public RCObject
+    {   
+        char* data;
+        stringvalue(const char *initvalue = "")
+        {
+            data = new char[strlen(initvalue) +1];
+            strcpy(data,initvalue);
+        }
+
+        stringvalue(const stringvalue& str)
+        {
+            data = new char[strlen(str.data) +1];
+            strcpy(data,str.data);
+        }
+
+        ~stringvalue()
+        {
+            delete [] data;
+        }
+    };
+    RCPtr<stringvalue> value;
+  public:
+    my_RCstring(const char* initvalue):value(new stringvalue(initvalue))
+    {
+
+    }
+
+    const char & operator[](int index) const
+    {
+        return value->data[index];
+    }
+    char & operator[](int index)
+    {
+        if (value->Isshared())
+        {
+            value = new stringvalue(value->data);
+        }
+        value->markUnshareable();
+        return value->data[index];
+    }
+};
+
+template <class T>
+class RCIPtr
+{
+  public:
+    RCIPtr(T *realPtr = 0);
+    RCIPtr(const RCIPtr &rhs);
+    ~RCIPtr();
+    RCIPtr &operator=(const RCIPtr &rhs);
+    const T *operator->() const; // see below for an
+    T *operator->();             // explanation of why
+    const T &operator*() const;  // these functions are
+    T &operator*();              // declared this way
+  private:
+    struct CountHolder : public RCObject
+    {
+        ~CountHolder() { delete pointee; }
+        T *pointee;
+    };
+    CountHolder *counter;
+    void init();
+    void makeCopy(); // see below
+};
+template <class T>
+void RCIPtr<T>::init()
+{
+    if (counter->isShareable() == false)
+    {
+        T *oldValue = counter->pointee;
+        counter = new CountHolder;
+        counter->pointee = new T(*oldValue);
+    }
+    counter->addReference();
+}
+template <class T>
+RCIPtr<T>::RCIPtr(T *realPtr)
+    : counter(new CountHolder)
+{
+    counter->pointee = realPtr;
+    init();
+}
+template <class T>
+RCIPtr<T>::RCIPtr(const RCIPtr &rhs)
+    : counter(rhs.counter)
+{
+    init();
+}
+template <class T>
+RCIPtr<T>::~RCIPtr()
+{
+    counter->removeReference();
+}
+template <class T>
+RCIPtr<T> &RCIPtr<T>::operator=(const RCIPtr &rhs)
+{
+    if (counter != rhs.counter)
+    {
+        counter->removeReference();
+        counter = rhs.counter;
+        init();
+    }
+    return *this;
+}
+template <class T>         // implement the copy
+void RCIPtr<T>::makeCopy() // part of copy-on-
+{                          // write (COW)
+    if (counter->isShared())
+    {
+        T *oldValue = counter->pointee;
+        counter->removeReference();
+        counter = new CountHolder;
+        counter->pointee = new T(*oldValue);
+        counter->addReference();
+    }
+}
+template <class T>                     // const access;
+const T *RCIPtr<T>::operator->() const // no COW needed
+{
+    return counter->pointee;
+}
+template <class T>         // non-const
+T *RCIPtr<T>::operator->() // access; COW
+{
+    makeCopy();
+    return counter->pointee;
+} // needed
+template <class T>                    // const access;
+const T &RCIPtr<T>::operator*() const // no COW needed
+{
+    return *(counter->pointee);
+}
+template <class T>        // non-const
+T &RCIPtr<T>::operator*() // access; do the
+{
+    makeCopy();
+    return *(counter->pointee);
+} // COW thing
 
 int main()
 {
