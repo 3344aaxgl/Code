673230883dc53ef755598433d39a31c4bd509b3c
3344aaxgl <343070926@qq.com>
2018-06-14 21:26:49 +0800

    2018-06-14原子操作
    原子操作

--- cpp_concurrency_in_action/5.cpp
+++ cpp_concurrency_in_action/5.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <atomic>
 #include <vector>
+#include <cassert>
 
 class spinclock_mutex
 {
@@ -26,7 +27,7 @@ class spinclock_mutex
 
 void fun(int n, spinclock_mutex &sp)
 {
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 10; i++)
     {
         sp.lock();
         std::cout << "Now it's thread " << n << '\n';
@@ -46,12 +47,73 @@ void test_spinlock()
       t.join();
 }
 
+class Foo
+{
+
+};
+
+std::shared_ptr<Foo> p;
+void process_global_data()
+{
+    std::shared_ptr<Foo> local = std::atomic_load(&p);
+}
+void update_global_data()
+{
+    std::shared_ptr<Foo> local(new Foo);
+    std::atomic_store(&p, local);
+}
+
+std::atomic_bool data_ready(false);
+std::vector<int> data;
+
+void reader_thread()
+{
+    while(!data_ready.load());
+    std::cout<<"The answer="<<data[0]<<'\n';
+}
+
+void writer_thread()
+{
+    data.push_back(42);
+    data_ready.store(true);
+}
+
+void fun(int a,int b)
+{
+    std::cout<<a<<" "<<b<<'\n';
+}
+
+int getnum()
+{
+    static int num = 0;
+    return ++num;
+}
+
 int main()
 {
     test_spinlock();
+
     bool expect = true;
     std::atomic_bool b;
     while(!b.compare_exchange_weak(expect,false))
       std::cout<<"do compare_exchange_weak";
+    
+    Foo f[5];
+    std::atomic<Foo*> p(f);
+    Foo* x = p.fetch_add(2);
+    assert(x == f);
+    assert(p.load() == &f[2]);
+    x = (p -= 1);
+    assert(x == &f[1]);
+    assert(p.load() == &f[1]);
+    
+    std::thread t1(reader_thread);
+    std::thread t2(writer_thread);
+
+    t1.join();
+    t2.join();
+
+    fun(getnum(),getnum());
+
     return 0;
 }
